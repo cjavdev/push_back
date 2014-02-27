621ed965545b0b75b85915c38ea78b718210325c
Ryan Sepassi <rsepassi@gmail.com>
2014-02-27 11:35:15 -0800

    Updated friending and unfriending logic

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -21,9 +21,32 @@ class Friendship < ActiveRecord::Base
     u1 = User.id_for(user_or_id1)
     u2 = User.id_for(user_or_id2)
 
-    Friendship.create!(
-      user_id: u1,
-      friend_id: u2
-    )
+    f1, f2 = nil, nil
+    Friendship.transaction do
+      f1 = Friendship.create!(
+        user_id: u1,
+        friend_id: u2
+      )
+
+      f2 = Friendship.create!(
+        user_id: u2,
+        friend_id: u1
+      )
+    end
+
+    f1
+  end
+
+  def self.destroy_friendship(user_or_id1, user_or_id2)
+    u1 = User.id_for(user_or_id1)
+    u2 = User.id_for(user_or_id2)
+
+    f1 = Friendship.find_by(:user_id => u1, :friend_id => u2)
+    f2 = Friendship.find_by(:user_id => u2, :friend_id => u1)
+
+    Friendship.transaction do
+      f1.destroy!
+      f2.destroy!
+    end
   end
 end
---
+++ db/migrate/20140227192414_remove_unique_index_on_friendships.rb
@@ -0,0 +1,9 @@
+class RemoveUniqueIndexOnFriendships < ActiveRecord::Migration
+  def up
+    remove_index :friendships, [:user_id, :friend_id]
+  end
+
+  def down
+    add_index :friendships, [:user_id, :friend_id], :unique => true
+  end
+end
