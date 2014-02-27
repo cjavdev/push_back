74d2314298def54f15af200c0ef299c712564c2b
Ryan Sepassi <rsepassi@gmail.com>
2014-02-27 11:35:15 -0800

    Updated friending and unfriending logic

--- app/models/concerns/user/friendable.rb
+++ app/models/concerns/user/friendable.rb
@@ -3,8 +3,7 @@ class User
     extend ActiveSupport::Concern
 
     included do
-      has_many :friendships, 
-        ->(user) { where("user_id = ? OR friend_id = ?", user.id, user.id) }
+      has_many :friendships
       has_many :friends, :through => :friendships
     end
 
@@ -13,12 +12,7 @@ class User
     end
 
     def unfriend(user_or_id)
-      user_or_id = User.id_for(user_or_id)
-
-      friendship = self.friendships
-                       .find_by("user_id = ? OR friend_id = ?", user_or_id, user_or_id)
-
-      friendship && friendship.destroy
+      Friendship.destroy_friendship(self, user_or_id)
     end
 
     module ClassMethods
