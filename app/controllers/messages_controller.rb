a51f26fc47d178e29041ace32273fba14a9d490d
Ryan Sepassi <rsepassi@gmail.com>
2014-02-27 15:35:22 -0800

    Messaging implemented

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,32 @@
 class MessagesController < ApplicationController
+  before_action :ensure_message_type, 
+                :ensure_friends,
+                :only => :create
+
+  def index
+    messages = current_user.conversation_with(params[:friend_id])
+    render :index, :locals => { :messages => messages }
+  end
+
+  def create
+    current_user.send(params[:message_type].to_sym, params[:friend_id], params[:body])
+    messages = current_user.conversation_with(params[:friend_id])
+    render :index, :locals => { :messages => messages }
+  end
+
+  private
+
+  def ensure_message_type
+    unless Message::MESSAGE_TYPES.include?(params[:message_type])
+      render :json => { :errors => ["Message type must be in [#{Message::MESSAGE_TYPES.join(', ')}]"] },
+             :status => :unprocessable_entity
+    end
+  end
+
+  def ensure_friends
+    unless current_user.friends_with?(params[:friend_id])
+      render :json => { :errors => ["You can only message people you're friends with."] },
+             :status => :unprocessable_entity
+    end
+  end
 end
--- app/models/concerns/user/friendable.rb
+++ app/models/concerns/user/friendable.rb
@@ -15,6 +15,11 @@ class User
       Friendship.destroy_friendship(self, user_or_id)
     end
 
+    def friends_with?(user_or_id)
+      user_or_id = User.id_for(user_or_id)
+      Friendship.exists?(:user_id => self.id, :friend_id => user_or_id)
+    end
+
     module ClassMethods
     end
   end
