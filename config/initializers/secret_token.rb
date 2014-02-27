ef31ac114117940389c44087fbfe95ee478f13db
CJ Avilla <cjavilla@gmail.com>
2014-02-26 16:34:53 -0800

    adding rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PushBack::Application.config.secret_key_base = 'eed3e2bf27bae03f8519adfc02aef8d381fb29a416df554c03a91f094a1eb47f6c4f3af4885d41b4f0334bb789f99b9e21022653a006860b62fb85ace4fc9143'
