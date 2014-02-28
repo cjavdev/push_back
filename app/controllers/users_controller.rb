3981f2bf8e5b202b7f397ca6017aa681a2c7aaca
Ryan Sepassi <rsepassi@gmail.com>
2014-02-28 12:03:26 -0800

    Fleshed out users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,34 @@
 class UsersController < ApplicationController
-  before_action :require_user
+  before_action :require_user, except: [:create]
 
   def show
-    @user = User.where(id: current_user.id)
-                .limit(1)
-                .includes(:workouts,
-                          friendships: [:friend],
-                          received_friend_requests: [:sender])
-                .first
-    render :show
+    user = User.where(id: current_user.id)
+               .limit(1)
+               .includes(:workouts,
+                         friendships: [:friend],
+                         received_friend_requests: [:sender])
+               .first
+    render :show, locals: { user: user }
+  end
+
+  def destroy
+    confirmation = params.require(:confirmation)
+    confirmation && current_user.destroy
+  end
+
+  def create
+    # Handle first-time Facebook Login
+    # find or create user
+    user = User.first
+    login(user)
+    render :show, locals: { user: user }
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
   end
 end
--- app/views/users/show.json.jbuilder
+++ app/views/users/show.json.jbuilder
@@ -1,15 +1,15 @@
-json.partial! 'users/user', user: @user
+json.partial! 'users/user', user: user
 
 json.friendships do
-  json.array! @user.friendships, partial: 'friendships/friendship', as: :friendship
+  json.array! user.friendships, partial: 'friendships/friendship', as: :friendship
 end
 
 json.workouts do
-  json.array! @user.workouts, partial: 'workouts/workout', as: :workout
+  json.array! user.workouts, partial: 'workouts/workout', as: :workout
 end
 
 json.received_friend_requests do
-  json.array! @user.received_friend_requests do |request|
+  json.array! user.received_friend_requests do |request|
     json.id request.id
     json.sender do
       json.partial! 'users/user', user: request.sender
