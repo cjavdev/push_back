4e9034fead296b69da033de94dacc385cdcfb052
Ryan Sepassi <rsepassi@gmail.com>
2014-02-28 07:05:07 -0800

    Added sessions controller and related

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,14 @@
 module SessionsHelper
+  def login(user)
+    token = user.reset_session_token!
+    session[:token] = token
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:token] = nil
+  end
+
   def logged_in?
     !!current_user
   end
@@ -6,6 +16,7 @@ module SessionsHelper
   def current_user
     @current_user ||= User.first
     # @current_user ||= User.find(2)
+    # @current_user ||= User.find_by(:session_token => session[:token])
   end
 
   def require_user
