979d3b2d3cd3b5897067647b6ef71efebd61f358
Ryan Sepassi <rsepassi@gmail.com>
2014-02-28 06:15:35 -0800

    Moved validation out of controller to model

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,8 +1,6 @@
 class FriendRequestsController < ApplicationController
   before_action :require_user
   before_action :ensure_recipient_id,
-                :ensure_recipient_exists,
-                :ensure_request_does_not_exist,
                 :only => [:create]
 
   def index
@@ -25,21 +23,5 @@ class FriendRequestsController < ApplicationController
 
   def ensure_recipient_id
     params.require(:recipient_id)
-  end
-
-  def ensure_recipient_exists
-    unless User.exists?(params[:recipient_id])
-      render :json => { :errors => ["User with id #{params[:recipient_id]} does not exist"]},
-             :status => :unprocessable_entity
-    end
-  end
-
-  def ensure_request_does_not_exist
-    request = FriendRequest.exist?(:sender_id => current_user.id
-                                   :recipient_id => params[:recipient_id])
-    if request
-      render :json => { :errors => ["Cannot request friendship twice"]},
-             :status => :unprocessable_entity
-    end
   end
 end
